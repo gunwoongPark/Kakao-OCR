37d9e43638afacc37528f239f397ddd2081d0867
gunwoongPark <olympiodoros@naver.com>
2021-03-14 14:34:28 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [source, setSource] = useState(null);
+  const [img, setImg] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const handleCapture = (target) => {
+    if (target.files) {
+      if (target.files.length !== 0) {
+        const file = target.files[0];
+        const newURL = URL.createObjectURL(file);
+        setImg(newURL);
+        setSource(file);
+      }
+    }
+  };
+
+  const kakaoOCR = () => {
+    let form = new FormData();
+    form.append("image", source);
+
+    axios
+      .post("https://dapi.kakao.com/v2/vision/text/ocr", form, {
+        headers: {
+          Authorization: "KakaoAK e2e76efe2b926e912bf4d8ed8879f5f2",
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        setResult(res.data.result[0].recognition_words[0]);
+      })
+      .catch((err) => {
+        setResult(err.response.statusText);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {source && (
+        <div className="imageContainer">
+          <img src={img} alt={"snap"} style={{ width: "100%" }}></img>
+        </div>
+      )}
+      {source && <button onClick={kakaoOCR}>Detect</button>}
+      <input
+        accept="image/*"
+        type="file"
+        capture="environment"
+        onChange={(e) => handleCapture(e.target)}
+      />
+      <div>{result}</div>
+    </>
   );
 }
 
